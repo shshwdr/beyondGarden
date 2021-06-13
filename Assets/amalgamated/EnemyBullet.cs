094869e5ea118ff265dc8a5108dc1bc8e0966169
shshwdr <cs.vivian04@gmail.com>
2021-06-13 00:04:38 -0500

    ghost

--- Assets/amalgamated/EnemyBullet.cs
+++ Assets/amalgamated/EnemyBullet.cs
@@ -8,7 +8,7 @@ public class EnemyBullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
